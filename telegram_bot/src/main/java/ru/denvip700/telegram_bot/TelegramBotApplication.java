444cec4a7b1d2701c2a4aa97bf283b0ccaead0a8
DenWorker <whiteskyD@yandex.ru>
2025-05-27 18:34:22 +0300

    Подключение eureka server

--- telegram_bot/src/main/java/ru/denvip700/telegram_bot/TelegramBotApplication.java
+++ telegram_bot/src/main/java/ru/denvip700/telegram_bot/TelegramBotApplication.java
@@ -2,8 +2,10 @@ package ru.denvip700.telegram_bot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class TelegramBotApplication {
 
     public static void main(String[] args) {
